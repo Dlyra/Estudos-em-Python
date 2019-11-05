e3de3612715a470eaea8542de5468251b13a4eab
D.Lyra <lyra_daniele@hotmail.com>
2019-11-05 19:21:58 -0300

    Verifica se o número é primo ou não

---
+++ numeros_primos.py
@@ -0,0 +1,16 @@
+
+x = int(input("Digite um número inteiro: "))
+
+if x == 2 or x == 3 or x == 5:
+    print ("primo")
+else:    
+    if x == 1 or x % 2 == 0 or x % 3 == 0 or x % 5 == 0:
+        print ("não primo")  
+    else:
+        print ("primo")
+    
+
+
+
+
+
