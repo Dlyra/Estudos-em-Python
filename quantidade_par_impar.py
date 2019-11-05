3b192af04c7d2aeabde1647c7e650433120afa9c
D.Lyra <lyra_daniele@hotmail.com>
2019-11-05 19:03:12 -0300

    Cálcula a quantidade de par e ímpar inseridos

---
+++ quantidade_par_impar.py
@@ -0,0 +1,14 @@
+terminou = False
+p = i = 0
+while (not terminou):
+    n = int(input("Digite um número, ou zero para terminar: "))
+    if n == 0:
+        terminou = True
+    else:
+        if n % 2 == 0:
+            p = p + 1
+        else:
+            i = i + 1
+
+print ("Quantidade de Números Pares = ", p)
+print ("Quantidade de Números Ímpares = ", i)
