d09d32f93ad8b1ec9acbfe2699ab39d888b9fb54
D.Lyra <lyra_daniele@hotmail.com>
2019-11-05 19:25:10 -0300

    Soma todos os valores inputados

---
+++ repticaosoma.py
@@ -0,0 +1,11 @@
+
+print ("Digite uma sequência de valores, finalize a soma digitando zero")
+soma = 0
+
+valor = 1 
+while valor != 0:
+    valor = int(input("Digite um valor a ser somado: "))
+    soma = soma + valor 
+    
+print ("A soma dos valores digitados é", soma)
+
