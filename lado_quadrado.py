d6d83ee4df2209931ae6facaf6f91b6521e823ac
D.Lyra <lyra_daniele@hotmail.com>
2020-06-29 18:41:39 -0300

    Calcula o lado do quadrado

---
+++ lado_quadrado.py
@@ -0,0 +1,6 @@
+ladoquadrado = input ("Digite o valor correspondente ao lado de um quadrado:")
+
+x = float (ladoquadrado) * 4
+y = float (ladoquadrado) ** 2
+
+print("perímetro:",int(x),"-","área:",int(y))
